e14faf37e91bb9868142b7090f05e0f06dd24a0f
WssIDs <wssids49@gmail.com>
2017-09-06 03:56:31 +0300

    Готовность КСР

--- Control_Work/Program.cs
+++ Control_Work/Program.cs
@@ -2,6 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 
+/*
+    Володько В.И. 60331-1 Вариант 2 
+
+    Предметная область: Вокзал. Касса вокзала имеет список тарифов на различные направления. При покупке билета регистрируются паспортные данные пассажира. Пассажир покупает билеты на различные направления. 
+        Все пассажиры имеют скидки. У одних пассажиров скидка задана в процентах, у других скидка фиксированная.
+        Добавить обработку исключительных ситуаций:
+    •    длина строки наименования тарифа больше 10 символов.
+    •    стоимость билета с учетом скидки меньше нуля.
+    Добавить перегруженный бинарный  оператор для уменьшения стоимости тарифа.
+    Система должна:
+    •    позволять вводить данные о тарифах;
+    •    позволять вводить паспортные данные пассажира и регистрировать покупку билета;
+    •    рассчитать среднюю стоимость проданных билетов;
+    •    по введенному наименованию направления высчитать сумму проданных билетов с учетом предоставленных скидок;
+
+*/
+
 namespace Control_Work
 {
     /// <summary>
@@ -9,22 +26,49 @@ namespace Control_Work
     /// </summary>
     public class Ticket : IEquatable<Ticket>
     {
-
-        public Ticket(string location, double cost)
+        /// <summary>
+        /// Констуктор класса Билет
+        /// </summary>
+        public Ticket(string name, string locationFrom, string locationTo, double cost)
         {
-            Location = location;
+            Name = name;
             Cost = cost;
+            LocationFrom = locationFrom;
+            LocationTo = locationTo;
         }
 
         /// <summary>
         /// Направление
         /// </summary>
-        public string Location { get; set; }
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Направление Откуда
+        /// </summary>
+        public string LocationFrom { get; set; }
+
+        /// <summary>
+        /// Направление Куда
+        /// </summary>
+        public string LocationTo { get; set; }
 
         /// <summary>
         /// Цена
         /// </summary>
         public double Cost { get; set; }
+
+        /// <summary>
+        /// Перегружаем бинарный оператор -
+        /// </summary>
+        public static Ticket operator -(Ticket obj1, int cost)
+        {
+            if (cost >= obj1.Cost) // если значение уменьшения стоимости тарифа >= тарифа
+                throw new InvalidOperationException("Тариф не может быть меньше либо равно нулю"); // вызываем исключение
+
+            obj1.Cost -= cost;
+
+            return obj1;
+        }
 
         /// <summary>
         /// Сравнение 
@@ -33,11 +77,11 @@ namespace Control_Work
         /// <returns></returns>
         public bool Equals(Ticket other)
         {
-            if (this.Location == other.Location)
+            if (this.Name == other.Name) // если имена совпадают
             {
                 return true;
             }
-            else
+            else // если имена не совпадают
             {
                 return false;
             }
@@ -48,7 +92,7 @@ namespace Control_Work
         /// </summary>
         public override string ToString()
         {
-            return string.Format("{0,10} |{1,4}\n", Location, Cost);
+            return string.Format("{0,10} | {1,10} | {2,10} |{3,4}\n", Name,LocationFrom,LocationTo,Cost);
         }
 
     }
@@ -58,6 +102,14 @@ namespace Control_Work
     /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Конструктор класса Пассажир
+        /// </summary>
+        /// <param name="FIO">ФИО</param>
+        /// <param name="seria">Серия паспорта</param>
+        /// <param name="passportnumber">Номер паспорта</param>
+        /// <param name="bpercentdiscount">Процентная скидка</param>
+        /// <param name="discountvalue">Скидка</param>
         public Person(string FIO, string seria, long passportnumber,bool bpercentdiscount,double discountvalue)
         {
             this.FIO = FIO;
@@ -107,7 +159,7 @@ namespace Control_Work
 
             temp += "Билеты:\n";
 
-            foreach (Ticket item in Tickets)
+            foreach (Ticket item in Tickets) // Проходим по коллекции Tickets в цикле
             {
                 temp+= item.ToString();
             }
@@ -116,39 +168,47 @@ namespace Control_Work
         }
 
         /// <summary>
-        /// Добавление билета
+        /// Добавление билета с учетом скидки
         /// </summary>
-        /// <param name="location">Направление</param>
-        /// <param name="cost">Цена</param>
-        /// <returns></returns>
-        public bool AddTicket(string location, double cost)
+        public bool AddTicket(Ticket ticket)
         {
-            if (discountValue >= 0)
+            if (discountValue >= 0) // если скидка >= 0
             {
-                if (bPercentDiscount)
+                if (bPercentDiscount) // используем процентную скидку
                 {
-                    cost -= discountValue*cost/100;
+                    if (discountValue <= 100) // если скидка <= 100%
+                    {
+                        ticket.Cost -= discountValue * ticket.Cost / 100;
+                    }
+                    else // если скидка > 100%
+                    {
+                        Console.WriteLine("Процентная скидка не может превышать больше 100%");
+                        return false;
+                    }
                 }
 
-                else
+                else // используем фиксированную скидку
                 {
-                    if ((cost - discountValue) >= 0)
+                    if ((ticket.Cost - discountValue) >= 0) // Разность цены билета и скидки >= 0
                     {
-                        cost -= discountValue;
+                        ticket.Cost -= discountValue;
                     }
 
-                    else
+                    else // Разность цены билета и скидки < 0
                     {
                         Console.WriteLine("Стоимость билета с учетом скидки меньше нуля \n Билет невозможно зарегистрировать");
                         return false;
                     }
                 }
 
-                Ticket ticket = new Ticket(location, cost);
-
-                Tickets.Add(ticket);
+                Tickets.Add(ticket); // Добавление билета в коллекцию
 
                 return true;
+            }
+
+            else // если скидка < 0
+            {
+                Console.WriteLine("Скидка не может быть меньше нуля");
             }
 
             return false;
@@ -177,13 +237,33 @@ namespace Control_Work
         /// </summary>
         /// <param name="cost">Цена</param>
         /// <param name="location">Направление</param>
-        public void AddTarif(double cost, string location)
+        public void AddTarif(double cost,string locationFrom, string locationTo, string name)
         {
-            Ticket tarif = new Ticket(location, cost);
-
-            if (!TarifList.Contains(tarif))
+            if (cost > 0) // Стоимость > 0
             {
-                TarifList.Add(tarif);
+                if (name.Length < 10) // Количество символов < 10
+                {
+                    Ticket tarif = new Ticket(name, locationFrom, locationTo, cost);
+
+                    if (!TarifList.Contains(tarif)) // если не содержит данного тарифа
+                    {
+                        TarifList.Add(tarif); // добавление тарифа в коллекцию
+                    }
+
+                    else // если данный тариф уже присутствует
+                    {
+                        Console.WriteLine("Тариф уже существует. Невозможно добавить тариф");
+                    }
+                }
+
+                else  // Количество символов >= 10
+                {
+                    Console.WriteLine("Количество символов названия тарифа должно быть меньше 10. Невозможно добавить тариф");
+                }
+            }
+            else // Стоимость <= 0
+            {
+                Console.WriteLine("Цена тарифа не может быть меньше либо равно нулю. Невозможно добавить тариф");
             }
         }
 
@@ -199,7 +279,7 @@ namespace Control_Work
         {
             Person person = new Person(fio, seria, passportnumber,bpercentdiscount,discountvalue);
 
-            PersonList.Add(person);
+            PersonList.Add(person); // Добавление пассажира в коллекцию
 
             return person;
         }
@@ -218,7 +298,7 @@ namespace Control_Work
         /// Получить сумму приобретенных билетов по направлению
         /// </summary>
         /// <param name="Location">Направление</param>
-        public double GetSumTicket(string Location)
+        public double GetSumTicket(string locationTo)
         {
             double sum = 0.0f;
 
@@ -226,9 +306,9 @@ namespace Control_Work
             {
                 foreach(Ticket ticket in person.Tickets)
                 {
-                    if(ticket.Location == Location)
+                    if(ticket.LocationTo == locationTo) // если направления совпадают
                     {
-                        sum += ticket.Cost;
+                        sum += ticket.Cost; // Высчитываем сумму билетов
                     }
                 }
             }
@@ -236,6 +316,10 @@ namespace Control_Work
                 return sum;
         }
 
+        /// <summary>
+        /// Расчет средней стоимости проданных билетов
+        /// </summary>
+        /// <returns>Средняя стоимость</returns>
         public double ShowAverangeCostTicket()
         {
             double sum = 0;
@@ -245,16 +329,16 @@ namespace Control_Work
             {
                 foreach(Ticket ticket in person.Tickets)
                 {
-                    sum += ticket.Cost;
-                    count++;
+                    sum += ticket.Cost; // сумма проданных билетов
+                    count++; // количество
                 }
             }
 
             double avsum = 0.0f;
 
-            if (count > 0)
+            if (count > 0) // если количество > 0
             {
-                avsum = sum / count;
+                avsum = sum / count; // расчет средней стоимости
             }
 
             Console.WriteLine("Расчет средней стоимости проданных билетов {0}",avsum);
@@ -262,11 +346,15 @@ namespace Control_Work
             return avsum;
         }
 
+        /// <summary>
+        /// Вывод списка тарифов и пассажирова с приобретенными билетами
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             string temp = string.Empty;
 
-            if (TarifList.Count > 0)
+            if (TarifList.Count > 0)  // если количество тарифов > 0
             {
                 foreach (Ticket tarif in TarifList)
                 {
@@ -274,12 +362,12 @@ namespace Control_Work
                 }
             }
 
-            else
+            else  // если количество тарифов <= 0
             {
                 Console.WriteLine("Список тарифов пуст");
             }
 
-            if (PersonList.Count > 0)
+            if (PersonList.Count > 0)  // если количество пассажиров > 0
             {
                 foreach (Person person in PersonList)
                 {
@@ -287,7 +375,7 @@ namespace Control_Work
                 }
             }
 
-            else
+            else  // если количество пассажиров <= 0
             {
                 Console.WriteLine("Список пассажиров пуст");
             }
@@ -303,13 +391,13 @@ namespace Control_Work
         {
             int i = 0;
 
-            if (TarifList.Count > 0)
+            if (TarifList.Count > 0) // если количество тарифов > 0
             {
-                foreach (var Item in TarifList)
+                foreach (Ticket Item in TarifList)
                 {
-                    Console.Write(i.ToString()+" "+Item.ToString());
+                    Console.Write(i.ToString()+" "+Item.ToString()); // Вывод определенного тарифа
 
-                    i++;
+                    i++; // счетчик
                 }
             }
         }
@@ -319,19 +407,19 @@ namespace Control_Work
     {
         static void Main(string[] args)
         {
-            Kassa kassa = new Kassa();
+            Kassa kassa = new Kassa(); // Создание объекта класса Kassa
 
-            while (true)
+            while (true) // пока true выводим меню
             {
                 int inputMenu = 0;
 
                 Console.WriteLine();
                 Console.WriteLine("МЕНЮ");
                 Console.WriteLine("1 - Добавить тариф");
-                Console.WriteLine("2 - Добавить пассажира и загеристрировать билет");
+                Console.WriteLine("2 - Добавить пассажира и зарегистрировать билет");
                 Console.WriteLine("3 - Рассчет средней стоимости проданных билетов");
                 Console.WriteLine("4 - Расчет суммы проданных билетов с учетом предоставленных скидок по введенному наименованию направления");
-                Console.WriteLine("5 - Вывести всю информацию по кассе");
+                Console.WriteLine("5 - Уменьшение стоимости тарифа по номеру тарифа");
                 Console.WriteLine("0 - Выход");
                 Console.Write("Выберите пункт меню: ");
 
@@ -339,22 +427,26 @@ namespace Control_Work
                 {
                     inputMenu = int.Parse(Console.ReadLine());
 
-                    if (inputMenu == 1)
+                    if (inputMenu == 1)   // если ввели 1
                     {
                         Console.WriteLine("Введите тариф");
-                        Console.Write("Направление:");
-                        string location = Console.ReadLine();
+                        Console.Write("Наименование:");
+                        string name = Console.ReadLine();
+                        Console.Write("Откуда:");
+                        string locationFrom = Console.ReadLine();
+                        Console.Write("Куда:");
+                        string locationTo = Console.ReadLine();
                         Console.Write("Цена:");
                         double cost = double.Parse(Console.ReadLine());
 
-                        kassa.AddTarif(cost, location);
+                        kassa.AddTarif(cost,locationFrom,locationTo, name);
 
 
                         Console.WriteLine();
                         Console.WriteLine("______________________________________________________");
                     }
 
-                    else if (inputMenu == 2)
+                    else if (inputMenu == 2)   // если ввели 2
                     {
                         Console.WriteLine("Введите пасспортные данные");
                         Console.Write("ФИО:");
@@ -369,12 +461,12 @@ namespace Control_Work
                         bool bpercentdiscount = false;
                         int discount = int.Parse(Console.ReadLine());
 
-                        if(discount == 0)
+                        if(discount == 0) // если скидка фиксированная
                         {
                             bpercentdiscount = false; 
                         }
 
-                        if (discount == 1)
+                        if (discount == 1)  // если скидка процентная
                         {
                             bpercentdiscount = true;
                         }
@@ -384,22 +476,23 @@ namespace Control_Work
 
                         Person person = kassa.AddPerson(fio, seria, passportnumber,bpercentdiscount,discountValue);
 
-                        while (true)
+                        while (true) // пока true выводим подменю
                         {
                             int inputsubMenu = 0;
 
                             Console.WriteLine();
                             Console.WriteLine("Регистрация билета:");
-                            Console.WriteLine("Выберите направление:");
+                            Console.WriteLine("Выберите тариф:");
 
-                            kassa.ShowListTarif();
+                            kassa.ShowListTarif(); // вывод списка тарифов
 
                             Console.WriteLine("для выхода из меню нажмите любой символ");
 
                             try
                             {
                                 inputsubMenu = int.Parse(Console.ReadLine());
-                                person.AddTicket(kassa.GetTarif(inputsubMenu).Location, kassa.GetTarif(inputsubMenu).Cost);
+
+                                person.AddTicket(kassa.GetTarif(inputsubMenu));
                             }
 
                             catch (FormatException ex)
@@ -410,13 +503,13 @@ namespace Control_Work
                         }
                     }
 
-                    else if (inputMenu == 3)
+                    else if (inputMenu == 3)  // если ввели 3
                     {
 
-                        kassa.ShowAverangeCostTicket();
+                        kassa.ShowAverangeCostTicket(); // вывод средней стомости проданных билетов
                     }
 
-                    else if (inputMenu == 4)
+                    else if (inputMenu == 4)  // если ввели 4
                     {
                         Console.Write("Введите направление:");
                         string location = Console.ReadLine();
@@ -424,12 +517,41 @@ namespace Control_Work
                         Console.WriteLine("Сумма билетов с направлением {0} - {1}",location,kassa.GetSumTicket(location));
                     }
 
-                    else if (inputMenu == 5)
+                    else if (inputMenu == 5)  // если ввели 5
                     {
-                        Console.WriteLine(kassa.ToString());
+
+                        while (true) // пока true выводим подменю
+                        {
+                            kassa.ShowListTarif(); // выаод списка тарифов
+
+                            int inputsubMenu = 0;
+
+                            Console.WriteLine();
+                            Console.WriteLine("Выберите тариф:");
+
+                            Console.WriteLine("для выхода из меню нажмите любой символ");
+
+                            try
+                            {
+                                inputsubMenu = int.Parse(Console.ReadLine()); 
+
+                                Ticket ticket = kassa.GetTarif(inputsubMenu); // получение тарифа по номеру
+
+                                Console.WriteLine("Уменьшение тарифа:");
+                                int lowCost = int.Parse(Console.ReadLine());
+
+                                ticket-=lowCost; // уменьшение тарифа
+                            }
+
+                            catch (FormatException ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                                break;
+                            }
+                        }
                     }
 
-                    else if (inputMenu == 0)
+                    else if (inputMenu == 0) // если ввели 0
                     {
                         break;
                     }
@@ -446,7 +568,7 @@ namespace Control_Work
 
             }
 
-            Console.ReadKey();
+            Console.ReadKey(); // ждем нажатия клавиши
     }
     }
 }
